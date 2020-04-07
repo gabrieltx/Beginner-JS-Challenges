f0bb8790576fb337216ca4a5007b79ce28cc3b15
Gabriel Teixeira <gabriel.teixeira94@gmail.com>
2020-04-07 14:47:15 -0300

    code added

---
+++ aposentadoria.js
@@ -0,0 +1,15 @@
+
+
+const nome = "Silvana";
+const sexo = "F";
+const idade = 60;
+const contribuicao = 43;
+
+const calcF = sexo == "F" && idade >= 30 && idade + contribuicao >= 85;
+const calcM = sexo == "M" && idade >= 35 && idade + contribuicao >= 95;
+
+if (calcF || calcM) {
+    console.log(`${nome}, você pode se aposentar!`)
+} else {
+    console.log(`${nome}, você ainda não pode se aposentar!`)
+}
