1eaadbb0d309aa9a12dfa950359719828df19a56
Gabriel Teixeira <gabriel.teixeira94@gmail.com>
2020-04-08 14:35:18 -0300

    code added

--- desafio3.js
+++ desafio3.js
@@ -1,13 +1,26 @@
-const usuarios = [
+    const usuarios = [
     { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
     { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
     { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
   ]
 
 
-  for (let i = 0; i < usuarios.length; i++) {
-      console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+//   for (let i = 0; i < usuarios.length; i++) {
+//       console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+//   }
+
+
+function CheckCSS (usuarios) {
+        User.CSS = false
+        if(usuarios[i].tecnologias = "CSS") {
+            User.CSS = true
+        }
+}
+
+for (let i = 0; i < usuarios.length; i++) {
+    const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuario[i]);
+  
+    if (usuarioTrabalhaComCSS) {
+      console.log(`O usuário ${usuario[i].nome} trabalha com CSS`);
+    }
   }
-
-
-
